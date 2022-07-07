6d7b1a6d2ebbbe6fa6974da54cb36bc1cd83d986
Thomas <thomasrnunez@gmail.com>
2022-07-07 00:08:56 -0400

    added postman and graphql changes

---
+++ LemonadeStand.Abstractions/Models/ProductMutation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LemonadeStand.Abstractions.Models
+{
+    public class ProductMutation
+    {
+		public int Id { get; set; }
+		public int SizeId { get; set; }
+		public int LemonadeTypeId { get; set; }
+		public double Amount { get; set; }
+
+		public ProductMutation()
+        {
+        }
+    }
+}
+
