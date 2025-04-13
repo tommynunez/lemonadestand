8590bfce51f04d214fa6b677400ac50407495040
tommynunez <thomasrnunez@gmail.com>
2025-04-13 00:44:14 -0400

    wip

--- LemonadeStand.IdentityServer/Services/AuthenticationService.cs
+++ LemonadeStand.IdentityServer/Services/AuthenticationService.cs
@@ -1,28 +1,41 @@
+using System.Net;
 using System.Security.Claims;
-using System.Threading.Tasks;
+using System.Web.Http;
+using LemonadeStand.IdentityServer.Data.Entities;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 
 namespace LemonadeStand.IdentityServer.Services
 {
   public interface IAuthenticationService
   {
-    Task Signin();
+    Task Signin(string? username, string? password);
   }
 
   public class AuthenticationService : IAuthenticationService
   {
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly SignInManager<AppUser> _signInManager; 
     public AuthenticationService(IHttpContextAccessor httpContextAccessor)
     {
       _httpContextAccessor = httpContextAccessor;
     }
 
-    public async Task Signin()
+    public async Task Signin(string? username, string? password)
     {
+      if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+      {
+          throw new HttpResponseException(HttpStatusCode.Unauthorized);
+      }
+
+      var isAuthenticated = await _signInManager.PasswordSignInAsync(username, password, true, true);
+      
+      
+
       await AddAuthenticatedUserClaim();
     }
-
+    
     private async Task AddAuthenticatedUserClaim()
     {
       var claims = new List<Claim>()
